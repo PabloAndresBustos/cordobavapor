62890929103afd8a64bf6f1571011cd877552770
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-03-01 14:18:27 -0300

    Subiendo el primer maquetado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarUpComponent } from './components/nav-bar-up/nav-bar-up.component';
+import { ContentComponent } from './components/content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, NavBarUpComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/nav-bar-up/nav-bar-up.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar-up.component.html',
+  styleUrl: './nav-bar-up.component.scss'
+})
+export class NavBarUpComponent {
+
+  
+
+}
